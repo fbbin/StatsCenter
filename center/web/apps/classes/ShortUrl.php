536ea9c73bd0009bf96c39948f81eba15e0ca7b9
bear <bear@cyberbox.org>
2015-07-15 16:52:10 +0800

    新增 shorturl class 和 functions..

---
+++ center/web/apps/classes/ShortUrl.php
@@ -0,0 +1,33 @@
+<?php
+namespace App;
+
+class ShortUrl
+{
+    // 53 个字母数字乱序
+    const ALPHABET = '4sF8y2KPuzRHixUtfGX3gcCTLhnASMe65NjpBw9YWDqbamEkQrd7J';
+    const BASE = 53; // strlen(self::ALPHABET)
+    const OFFSET = 10000;
+
+    public static function encode($num)
+    {
+        $num = $num + self::OFFSET;
+        $str = '';
+        while ($num > 0)
+        {
+            $str = substr(self::ALPHABET, ($num % self::BASE), 1) . $str;
+            $num = floor($num / self::BASE);
+        }
+        return $str;
+    }
+    public static function decode($str)
+    {
+        $num = 0;
+        $len = strlen($str);
+        for ($i = 0; $i < $len; $i++)
+        {
+            $num = $num * self::BASE + strpos(self::ALPHABET, $str[$i]);
+        }
+        $num = $num - self::OFFSET;
+        return $num;
+    }
+}
