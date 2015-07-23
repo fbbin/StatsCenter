79fed4e7918a72b2aa1b09adaabc5e55f9502809
chendongxiong <chendongxiong@chelun.com>
2015-07-23 14:13:16 +0800

    去掉之前统计访问总数的逻辑

--- center/web/apps/models/Url_shortener.php
+++ center/web/apps/models/Url_shortener.php
@@ -102,23 +102,6 @@ class Url_shortener extends Swoole\Model
         return (bool) $res;
     }
 
-    function get_total_visits_list(array $symbol_list)
-    {
-        $key_list = $this->lastest_keys_of_visits(180);
-        // 将近180天的访问次数累加
-        $this->swoole->redis('cluster')->zUnion('tiny-url:visits:sum', $key_list);
-
-        $total_visits_list = array();
-        foreach ($symbol_list as $symbol)
-        {
-            $total_visits = $this->swoole->redis('cluster')->zScore('tiny-url:visits:sum', $symbol);
-            $total_visits = $total_visits !== false ? $total_visits : 0;
-            $total_visits_list[$symbol] = $total_visits;
-        }
-
-        return $total_visits_list;
-    }
-
     function get_tiny_url_list(array $symbol_list)
     {
         $list = array();
