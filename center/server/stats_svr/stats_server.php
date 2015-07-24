88369ba8dd0ff0dbd27876760b7128276834ef20
matyhtf <mikan.tenny@gmail.com>
2015-07-24 16:30:03 +0800

    迁移目录

---
+++ center/server/stats_svr/stats_server.php
@@ -0,0 +1,15 @@
+<?php
+define('DEBUG', 'on');
+define('WEBPATH', __DIR__);
+require dirname(__DIR__) . '/config.php';
+
+$setting['worker_num'] = 24;
+$setting['task_worker_num'] = 24;
+$setting['daemonize'] = false;
+$setting['pid_file'] = __DIR__.'/dump/server.pid';
+$setting['worker_dump_file'] = __DIR__.'/dump/worker';
+$setting['task_dump_file'] = __DIR__.'/dump/task';
+$svr = new App\StatsCenterSvr();
+$svr->setLogger(new Swoole\Log\FileLog(__DIR__.'/logs/server.log'));
+$svr->run($setting);
+
