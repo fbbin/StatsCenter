6356da953df39ae3b0bd5d0be4f68ffd66986054
chendongxiong <chendongxiong@chelun.com>
2015-08-14 15:29:20 +0800

    暂且允许所有人上传

--- center/web/apps/templates/include/leftmenu.php
+++ center/web/apps/templates/include/leftmenu.php
@@ -88,7 +88,7 @@
             </li>
         </ul>
     </li>
-    <?php if ($this->userinfo['usertype'] == 0 || $this->userinfo['usertype'] == 4) : ?>
+    <?php // if ($this->userinfo['usertype'] == 0 || $this->userinfo['usertype'] == 4) : ?>
     <li>
       <a href="#">
         <i class="fa fa-lg fa-fw fa-file"></i>
@@ -110,6 +110,6 @@
         </li>
       </ul>
     </li>
-     <?php endif; ?>
+     <?php // endif; ?>
 </ul>
 </nav>
