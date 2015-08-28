298995edf315fe97d57ebe534ed1188fcfb334d8
chendongxiong <chendongxiong@chelun.com>
2015-08-28 15:52:27 +0800

    新增接口列表 tpl...

--- center/web/apps/controllers/App_host.php
+++ center/web/apps/controllers/App_host.php
@@ -21,13 +21,13 @@ class App_host extends \App\LoginController
 
     function add_host()
     {
-        $form['symbol'] = \Swoole\Form::input('symbol');
         $form['name'] = \Swoole\Form::input('name');
+        $form['identifier'] = \Swoole\Form::input('identifier');
         $form['host'] = \Swoole\Form::input('host');
 
         $this->assign('form', $form);
 
-        $this->display('app_host/edit.php');
+        $this->display('app_host/edit_host.php');
     }
 
     function host_list()
