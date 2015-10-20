9d78b962c5c96c136e2da86e4611320938dad422
matyhtf <mikan.tenny@gmail.com>
2015-10-20 17:10:19 +0800

    数据库配置

--- center/server/aopnet_svr/aopnet_server.php
+++ center/server/aopnet_svr/aopnet_server.php
@@ -9,6 +9,11 @@ $setting['daemonize'] = false;
 
 ini_set('error_log', __DIR__.'/logs/php_errors.log');
 
+if (get_cfg_var('env.name') == 'local' or get_cfg_var('env.name') == 'dev')
+{
+    Swoole::$php->config->setPath(__DIR__.'/apps/configs/dev/');
+}
+
 $svr = new StatsCenter\AopNetServer();
 $svr->setLogger(new Swoole\Log\FileLog(__DIR__.'/logs/server.log'));
 $setting['pid_file'] = __DIR__.'/logs/server.pid';
