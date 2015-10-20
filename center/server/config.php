9cacb24f916e39f4bc794a2c165c6a0451cd09d5
matyhtf <mikan.tenny@gmail.com>
2015-10-20 17:04:15 +0800

    数据库配置

--- center/server/config.php
+++ center/server/config.php
@@ -1,5 +1,11 @@
 <?php
 require __DIR__.'/../../framework/libs/lib_config.php';
 Swoole\Loader::addNameSpace('StatsCenter', __DIR__ . '/classes');
+
 global $php;
 $php = Swoole::getInstance();
+
+if (get_cfg_var('env.name') == 'local' or get_cfg_var('env.name') == 'dev')
+{
+    $php->config->setPath(__DIR__.'/apps/configs/dev/');
+}
