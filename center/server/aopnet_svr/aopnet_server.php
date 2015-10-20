2473e45a631af96ae75a456653d0af0063e771d5
matyhtf <mikan.tenny@gmail.com>
2015-10-20 17:06:31 +0800

    配置PHP错误日志

--- center/server/aopnet_svr/aopnet_server.php
+++ center/server/aopnet_svr/aopnet_server.php
@@ -5,7 +5,9 @@ define('WEBPATH', __DIR__);
 require dirname(__DIR__) . '/config.php';
 
 $setting['worker_num'] = 4;
-$setting['daemonize'] = 1;
+$setting['daemonize'] = false;
+
+ini_set('error_log', __DIR__.'/logs/php_errors.log');
 
 $svr = new StatsCenter\AopNetServer();
 $svr->setLogger(new Swoole\Log\FileLog(__DIR__.'/logs/server.log'));
