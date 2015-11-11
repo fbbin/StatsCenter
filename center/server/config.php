9d63b6fe396d0bc25170f54b084a44f42f652eaa
matyhtf <mikan.tenny@gmail.com>
2015-11-11 17:43:11 +0800

    配置框架路径

--- center/server/config.php
+++ center/server/config.php
@@ -1,5 +1,9 @@
 <?php
-require __DIR__.'/../../framework/libs/lib_config.php';
+define('PUBLIC_PATH', '/data/www/public/');
+define('FRAMEWORK_PATH', PUBLIC_PATH.'/framework');
+define('ENV_NAME', $env);
+
+require FRAMEWORK_PATH . '/libs/lib_config.php';
 Swoole\Loader::addNameSpace('StatsCenter', __DIR__ . '/classes');
 
 global $php;
