aca89740ff5b9b52b68aec40aa2e348fc77e8fc3
matyhtf <mikan.tenny@gmail.com>
2015-11-26 14:32:18 +0800

    g

--- center/server/config.php
+++ center/server/config.php
@@ -8,3 +8,13 @@ Swoole\Loader::addNameSpace('StatsCenter', __DIR__ . '/classes');
 
 global $php;
 $php = Swoole::getInstance();
+
+$env = get_cfg_var('env.name');
+if ($env == 'dev' or $env == 'test' or $env == 'local')
+{
+    Swoole::$php->config->setPath(WEBPATH . '/apps/configs/dev');
+}
+else
+{
+    Swoole::$php->config->setPath(WEBPATH . '/apps/configs/product');
+}
