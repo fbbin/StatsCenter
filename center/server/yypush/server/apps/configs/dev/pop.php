3c06cb23619aa7a2ef66f15d49d19794ff50b853
matyhtf <mikan.tenny@gmail.com>
2015-11-26 14:41:27 +0800

    用户名密码

--- center/server/yypush/server/apps/configs/dev/pop.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pop['master'] = array(
-    'post' => 'http://122.224.255.103:53880/sysmsg/SysMsgIntf',
-);
-return $pop;
--- center/server/yypush/server/apps/configs/redis.php
+++
@@ -1,7 +0,0 @@
-<?php
-$redis['master'] = array(
-    'host' => "localhost",
-    'port' => 6379,
-    'database' => 15,
-);
-return $redis;
