efebdd63c548afec759d15ddb0df4f1f1f08895f
chendongxiong <chendongxiong@chelun.com>
2015-12-30 20:50:21 +0800

    导航..

--- center/web/apps/templates/include/leftmenu.php
+++ center/web/apps/templates/include/leftmenu.php
@@ -131,7 +131,7 @@
                 <a href="/app_host/rule_list/"><i class="fa fa-lg fa-fw fa-reorder"></i> <span class="menu-item-parent">指定列表</span></a>
             </li>
             -->
-            <li <?php if ($this->isActiveMenu('app_release', 'release_list')){ ?>
+            <li <?php if ($this->isActiveMenu('app_release', 'app_list')){ ?>
                 class="active" <?php } ?>>
                 <a href="/app_release/app_list/"><i class="fa fa-lg fa-fw fa-reorder"></i> <span class="menu-item-parent">APP版本管理</span></a>
             </li>
