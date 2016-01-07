2284ced19b3dbf05a8c7ef94a454b141d98bf8d2
matyhtf <mikan.tenny@gmail.com>
2016-01-07 17:28:46 +0800

    客户端上报

--- center/server/app_stats/server.php
+++ center/server/app_stats/server.php
@@ -4,7 +4,9 @@ require __DIR__ . '/_init.php';
 
 $setting['worker_num'] = 4;
 //$setting['daemonize'] = 1;
+StatsCenter\AppStatsServer::parserUrl();
 
+die;
 $svr = new StatsCenter\AppStatsServer();
 $svr->setLogger(new Swoole\Log\FileLog(__DIR__ . '/logs/app.log'));
 $setting['pid_file'] = __DIR__ . '/logs/server.pid';
