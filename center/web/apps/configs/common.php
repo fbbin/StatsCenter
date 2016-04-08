eb6c5b52db8cbfa641fc6567315f52476cfa7da0
matyhtf <mikan.tenny@gmail.com>
2016-04-08 16:10:32 +0800

    拓扑图

--- center/web/apps/configs/common.php
+++ center/web/apps/configs/common.php
@@ -1,4 +1,11 @@
 <?php
 $array['site_name'] = '模调统计系统';
 $array['logo_url'] = '/static/image/logo.gif';
+$array['node_categorys'] = array(
+    'Web',
+    'Service',
+    'Redis',
+    'Memcache',
+    'MySQL'
+);
 return $array;
