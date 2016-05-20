2b4fe969f008f9b0431f56e02073a14747712927
严春昊 <yanchunhao@chelun.com>
2016-05-20 10:22:34 +0800

    还原db

--- center/web/apps/configs/product/db.php
+++ center/web/apps/configs/product/db.php
@@ -1,5 +1,5 @@
 <?php
-$db['master'] = array(
+/*$db['master'] = array(
     'type' => Swoole\Database::TYPE_CLMysql,
     'host' => "192.168.1.105",
     'port' => 9703,
@@ -39,45 +39,45 @@ $db['log_center'] = array(
     'charset' => "utf8",
     'setname' => true,
     'persistent' => true,
-);
+);*/
 
-/*$db['master'] = array(
-    'type'    => Swoole\Database::TYPE_MYSQLi,
-    'host'    => "192.168.1.102",
-    'port'    => 3306,
-    'dbms'    => 'mysql',
-    'engine'  => 'MyISAM',
-    'user'    => "mostat",
-    'passwd'  => "bufferme",
-    'name'    => "mostat",
-    'charset' => "utf8",
-    'setname' => true,
+$db['master'] = array(
+	'type' => Swoole\Database::TYPE_MYSQLi,
+	'host' => "192.168.1.102",
+	'port' => 3306,
+	'dbms' => 'mysql',
+	'engine' => 'MyISAM',
+	'user' => "mostat",
+	'passwd' => "bufferme",
+	'name' => "mostat",
+	'charset' => "utf8",
+	'setname' => true,
 );
 
 $db['platform'] = array(
-    'type'    => Swoole\Database::TYPE_MYSQLi,
-    'host'    => "192.168.1.212",
-    'port'    => 3306,
-    'dbms'    => 'mysql',
-    'engine'  => 'MyISAM',
-    'user'    => "platform",
-    'passwd'  => "Emuo0koo",
-    'name'    => "platform",
-    'charset' => "utf8",
-    'setname' => true,
+	'type' => Swoole\Database::TYPE_MYSQLi,
+	'host' => "192.168.1.212",
+	'port' => 3306,
+	'dbms' => 'mysql',
+	'engine' => 'MyISAM',
+	'user' => "platform",
+	'passwd' => "Emuo0koo",
+	'name' => "platform",
+	'charset' => "utf8",
+	'setname' => true,
 );
 
 $db['log_center'] = array(
-    'type'    => Swoole\Database::TYPE_MYSQLi,
-    'host'    => "192.168.1.54",
-    'port'    => 3306,
-    'dbms'    => 'mysql',
-    'engine'  => 'MyISAM',
-    'user'    => "log_center",
-    'passwd'  => "logdb@123cl",
-    'name'    => "log_center",
-    'charset' => "utf8",
-    'setname' => true,
-);*/
+	'type' => Swoole\Database::TYPE_MYSQLi,
+	'host' => "192.168.1.54",
+	'port' => 3306,
+	'dbms' => 'mysql',
+	'engine' => 'MyISAM',
+	'user' => "log_center",
+	'passwd' => "logdb@123cl",
+	'name' => "log_center",
+	'charset' => "utf8",
+	'setname' => true,
+);
 
 return $db;
