8af9a9dc0c0c9d6da0d9e02a4e644d543aca4657
chendongxiong <chendongxiong@chelun.com>
2016-05-26 11:03:30 +0800

    模调redis配置改成下发

--- center/web/apps/configs/product/redis.php
+++ center/web/apps/configs/product/redis.php
@@ -1,21 +1,3 @@
 <?php
-$redis['master'] = array(
-    'host' => "127.0.0.1",
-    'port' => 6379,
-    'database' => 15,
-);
 
-/**
- * Codis集群服务器
- */
-$redis['cluster'] = array(
-    'host' => "192.168.1.244",
-    'port' => 19000,
-);
-
-$redis['platform'] = array(
-    'host' => '192.168.1.73',
-    'port' => 19006,
-);
-
-return $redis;
+return \CloudConfig::getRedisConfig('platform/mostats-redis-config');
